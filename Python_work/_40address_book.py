09fc8905414b6cb1ea1dee5cf6ba23337bd38434
damonx <damon_xie@limei-trading.com>
2019-08-16 11:46:48 +0800

    练习程序的修改

--- Python_work/_40address_book.py
+++ Python_work/_40address_book.py
@@ -1,5 +1,6 @@
 # -- coding: utf-8 --
 # 需求：做一个通讯录，人的姓名、电话、地址，支持增删改
+import os
 import pickle
 
 
@@ -14,32 +15,43 @@ class address_book:
         return self.name + ',' + self.phone + ',' + self.address
 
 
+def isNotEmpty(s):
+    return bool(s and s.strip())
+
 addressFile = 'addressFile.data'
 address_list = {}
-f = open(addressFile, 'rb')
-address_list = pickle.load(f)
-print(address_list)
-f.close()
+if os.path.exists(addressFile):
+    with open(addressFile, 'rb') as f:
+        address_list = pickle.load(f)
+        for name, address in address_list.items():
+            print('联系人： {} 通讯信息： {}'.format(name, address))
+else:
+    os.system(r'touch {}'.format(addressFile))
+
 while True:
-    ctr = str(input('add?del?'))
+    ctr = str(input('add?del?exit?'))
     if ctr == 'add':
         name = str(input('please add name:'))
         phone = str(input('please add phone:'))
         address = str(input('please add address:'))
         a = address_book(name, phone, address)
         address_list[a.name] = a.getDetail()
-        print('new:', address_list)
-        f = open(addressFile, 'wb')
-        pickle.dump(address_list, f)
-        f.close()
-        with open(addressFile, 'rb') as f:
-            print(pickle.load(f))
+        with open(addressFile, 'wb') as f:
+            pickle.dump(address_list, f)
+            for name, address in address_list.items():
+                print('联系人： {} 通讯信息： {}'.format(name, address))
 
     elif ctr == 'del':
-        print('....')
-    elif ctr =='exit':
+        del_item = str(input('del name:'))
+        if isNotEmpty(del_item):
+            del address_list[del_item]
+            for name, address in address_list.items():
+                print('联系人： {} 通讯信息： {}'.format(name, address))
+    elif ctr == 'exit':
         print('bye~')
         break
     else:
         with open(addressFile, 'rb') as f:
-            print(pickle.load(f))
+            list = pickle.load(f)
+            for name, address in list.items():
+                print('联系人： {} 通讯信息： {}'.format(name, address))
